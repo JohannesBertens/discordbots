8776bf019216340f58f18ef55951349df7db6dd0
Johannes Bertens <johannes.bertens@gmail.com>
2019-08-09 08:29:03 +0200

    Added initial bots

---
+++ wiki/bot.js
@@ -0,0 +1,62 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+const request = require('request');
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '.') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // .ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+                break;
+            case 'wiki':
+                var url ='https://wotmud.fandom.com/api.php?action=opensearch&limit=5&search='+args[0] ;
+                //console.log(url);
+                request(url, {rejectUnauthorized: false, json: true }, (err, res, body) => {
+                    var links = body[1].map(result => 'https://wotmud.fandom.com/wiki/'+encodeURIComponent(result));
+                    console.log(links);
+                    
+                    if (err) { return console.log(err); }
+                    if (links.length > 0) {
+                        bot.sendMessage({
+                            to: channelID,
+                            message: links.join("\n")
+                        })
+                    } else {
+                        bot.sendMessage({
+                            to: channelID,
+                            message: 'No results for ' + args[0]
+                        })
+                    }
+                });               
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
