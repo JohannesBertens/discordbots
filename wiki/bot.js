d554aaab7ead5a6b2a14a1fc2d6854a7fd326da6
Johannes Bertens <johannes.bertens@gmail.com>
2020-05-10 17:30:15 +0200

    Spread out retrieval of information to different functions, added help.

--- wiki/bot.js
+++ wiki/bot.js
@@ -3,6 +3,33 @@ var logger = require('winston');
 var auth = require('./auth.json');
 const request = require('request');
 
+const statsHelpMessage="```" + `
+Usage:
+.stats help:            Display this message
+.stats <keywords>:      Display info on the WEAPONS with the keywords in the name
+.stats class <keyword>: Display info the CLASS of WEAPONS with the keyword in the name of the CLASS
+.stats eq <keywords>:   Display info on the EQUIPMENT with the keywords in the name
+` + "```";
+const angrealMessage="```" + `
+--- female angreal ---     lbs uses SPpu SPtotal
+a disc of moonstone            0.2   10   8     80
+a greenish, translucent sphere 1.0   10  10    100
+a necklace of fire sapphire    0.1    8   5     40
+a sculpted statuette           1.0   20   9    180
+a small crystalline circlet    0.2  150   3    450
+a wood carving of a bear       3.0    6   5     30
+an ivory figurine              1.0    8   4     32
+            
+ --- male angreal ---      lbs uses SPpu SPtotal
+a jade carving of an old man   0.5  50    9    450 
+a many-faceted stone	       4.0 142    3    426
+a small stone dagger	       ???  30    5    150
+a small crystalline globe      0.5  10   10    100
+a short, thick rod             2.5  10    6     60
+a carved ivory pyramid         ???  15   12    180` + "```";
+const pracMessage = 'http://ickmund.github.io/practrainer/';
+const eqMessage = 'https://docs.google.com/spreadsheets/d/1F7WvYpa45zZpJhqLeh747CgDtk0LJZg-LRJDXwRf-TU/edit#gid=0';
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -20,7 +47,7 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
-function printResults (body, bot, channelID) {
+function printResults (body) {
     var maxNameLength = 0;
     var maxTypeLength = 0;
     body.forEach(result => {
@@ -40,12 +67,66 @@ function printResults (body, bot, channelID) {
     });
 
     resultString += "```";
-    bot.sendMessage({
-        to: channelID,
-        message: resultString
-    })
+    return resultString;
 }
 
+function getWikiInfo(searchString, callback) {
+    var url='https://wotmud.fandom.com/api/v1/Search/List?query='+searchString+'&limit=1';
+    console.log(url);
+    request(url, {rejectUnauthorized: false, json: true }, (err, res, body) => {
+        if (err) { callback(err); return console.log(err); }
+        
+        var message = "";
+        var links = [];
+        if (body != undefined && body.items != undefined) {
+            body.items.forEach(item => {
+                links.push(item.url);
+            });
+        }
+
+        if (links.length > 0) {
+            message = links.join("\n");
+        } else {
+            message = 'No results for ' + searchString;
+        }
+        callback(message);
+    });             
+}
+
+function getStatsInfo(args, callback) {
+    if (args[0] == 'help') {
+        callback(statsHelpMessage);
+    } else if (args[0] == 'eq') {
+        // SEARCH FOR EQ HERE!
+    } else if (args[0] == 'class') {
+        var url='https://equipmentstats.azurewebsites.net/api/GetStats?type='+args[1];
+        console.log(url);
+        request(url, {rejectUnauthorized: false, json: true }, (err, res, body) => {
+            if (err) { return console.log(err); }
+            if (body.length == 0)
+            {
+                callback('No results for ' + args.join(' '));
+                return;
+            }
+
+            callback(printResults(body));                   
+        }); 
+    } else {
+        var url='https://equipmentstats.azurewebsites.net/api/GetStats?name='+args.join('%20');
+        console.log(url);
+        request(url, {rejectUnauthorized: false, json: true }, (err, res, body) => {
+            //console.log(body);
+            if (err) { return console.log(err); }
+            if (body.length == 0)
+            {
+                callback('No results for ' + args.join(' '));
+                return;
+            }
+
+            callback(printResults(body));
+        });
+    };              
+}
 
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
@@ -58,112 +139,26 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         switch(cmd) {
             // .ping
             case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
+                bot.sendMessage({to: channelID, message: 'Pong!'});
                 break;
             case 'prac':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'http://ickmund.github.io/practrainer/'
-                });
+                bot.sendMessage({to: channelID, message: pracMessage});
                 break;
             case 'eq':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'https://docs.google.com/spreadsheets/d/1F7WvYpa45zZpJhqLeh747CgDtk0LJZg-LRJDXwRf-TU/edit#gid=0'
-                });
+                bot.sendMessage({to: channelID, message: eqMessage});
                 break;
             case 'wiki':
-                var url='https://wotmud.fandom.com/api/v1/Search/List?query='+args.join('%20')+'&limit=1';
-                console.log(url);
-                request(url, {rejectUnauthorized: false, json: true }, (err, res, body) => {
-                    var links = [];
-                    if (body == undefined || body.items == undefined) {
-                        bot.sendMessage({
-                            to: channelID,
-                            message: 'No results for ' + args[0]
-                        })
-                    } else {
-                        body.items.forEach(item => {
-                            links.push(item.url);
-                        });
-                        
-                        if (err) { return console.log(err); }
-                        if (links.length > 0) {
-                            bot.sendMessage({
-                                to: channelID,
-                                message: links.join("\n")
-                            })
-                        } else {
-                            bot.sendMessage({
-                                to: channelID,
-                                message: 'No results for ' + args[0]
-                            })
-                        }
-                    }
-                });               
-                break;
-            case 'angreal':
-                var angrealMessage="```" + `
-    --- female angreal ---     lbs uses SPpu SPtotal
-a disc of moonstone            0.2   10   8     80
-a greenish, translucent sphere 1.0   10  10    100
-a necklace of fire sapphire    0.1    8   5     40
-a sculpted statuette           1.0   20   9    180
-a small crystalline circlet    0.2  150   3    450
-a wood carving of a bear       3.0    6   5     30
-an ivory figurine              1.0    8   4     32
-                
-     --- male angreal ---      lbs uses SPpu SPtotal
-a jade carving of an old man   0.5  50    9    450 
-a many-faceted stone	       4.0 142    3    426
-a small stone dagger	       ???  30    5    150
-a small crystalline globe      0.5  10   10    100
-a short, thick rod             2.5  10    6     60
-a carved ivory pyramid         ???  15   12    180` + "```";
-                bot.sendMessage({
-                    to: channelID,
-                    message: angrealMessage
+                getWikiInfo(args.join('%20'), (message) => {
+                    bot.sendMessage({to: channelID, message: message});
                 });
                 break;
+            case 'angreal':
+                bot.sendMessage({to: channelID, message: angrealMessage});
+                break;
             case 'stats':
-                if (args[0] == 'class') {
-                    var url='https://equipmentstats.azurewebsites.net/api/GetStats?type='+args[1];
-                    console.log(url);
-                    request(url, {rejectUnauthorized: false, json: true }, (err, res, body) => {
-                        //console.log(body);
-                        if (err) { return console.log(err); }
-                        if (body.length == 0)
-                        {
-                            bot.sendMessage({
-                                to: channelID,
-                                message: 'No results for ' + args.join(' ')
-                            });
-                            return;
-                        }
-
-                        printResults(body, bot, channelID);                        
-                    });   
-                } else {
-                    var url='https://equipmentstats.azurewebsites.net/api/GetStats?name='+args.join('%20');
-                    console.log(url);
-                    request(url, {rejectUnauthorized: false, json: true }, (err, res, body) => {
-                        //console.log(body);
-                        if (err) { return console.log(err); }
-                        if (body.length == 0)
-                        {
-                            bot.sendMessage({
-                                to: channelID,
-                                message: 'No results for ' + args.join(' ')
-                            });
-                            return;
-                        }
-
-                        printResults(body, bot, channelID);
-                    });              
-                }
+                getStatsInfo(args, (message) => {
+                    bot.sendMessage({to: channelID, message: message})
+                });
                 break;
             // Just add any case commands if you want to..
          }
