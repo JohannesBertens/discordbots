178ab28d8175519a5da5ce89b8ada7e7211666d3
Johannes <johannes.bertens@gmail.com>
2021-05-28 10:51:21 +0200

    Fix for WIKI?

--- wiki/bot.js
+++ wiki/bot.js
@@ -1,4 +1,5 @@
-var Discord = require('discord.io');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 var logger = require('winston');
 var auth = require('./auth.json');
 const request = require('request');
@@ -47,15 +48,10 @@ logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
+
+client.on('ready', function (evt) {
     logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info(`Logged in as ${client.user.tag}!`);
 });
 
 function printResults (body) {
@@ -353,44 +349,46 @@ function getAuctionInfo(args, callerId, callback) {
     }
 }
 
-bot.on('message', function (user, userID, channelID, message, evt) {
+client.on('message', msg => {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '.') {
-        var args = message.substring(1).split(' ');
+    if (msg.content.substring(0, 1) == '.') {
+        var args = msg.content.substring(1).split(' ');
         var cmd = args[0];
        
         args = args.splice(1);
         switch(cmd) {
             // .ping
             case 'ping':
-                bot.sendMessage({to: channelID, message: 'Pong!'});
+                msg.channel.send('Pong!');
                 break;
             case 'prac':
-                bot.sendMessage({to: channelID, message: pracMessage});
+                msg.channel.send(pracMessage);
                 break;
             case 'eq':
-                bot.sendMessage({to: channelID, message: eqMessage});
+                msg.channel.send(eqMessage);
                 break;
             case 'wiki':
                 getWikiInfo(args.join('%20'), (message) => {
-                    bot.sendMessage({to: channelID, message: message});
+                    msg.channel.send(message);
                 });
                 break;
             case 'angreal':
-                bot.sendMessage({to: channelID, message: angrealMessage});
+                msg.channel.send(angrealMessage);
                 break;
             case 'stats':
                 getStatsInfo(args, (message) => {
-                    bot.sendMessage({to: channelID, message: message})
+                    msg.channel.send(message)
                 });
                 break;
             case 'auction':
                 getAuctionInfo(args, userID, (message) => {
-                    bot.sendMessage({to: channelID, message: message})
+                    msg.channel.send(message)
                 });
                 break;
             // Just add any case commands if you want to..
          }
      }
 });
+
+client.login(auth.token)
